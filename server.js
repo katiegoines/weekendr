29c093b2b32f1cb971460d74e68b4dbfc2a5702f
Katie Goines <katiegoines3@gmail.com>
2017-11-06 14:58:41 -0800

    heroku deployment code

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ mongoose.connect(MONGODB_URI, (err) => {
     console.log(err || "Connected to MongoDB.")
 })
 
+app.use(express.static(`${__dirname}/client/build`))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 
@@ -51,6 +52,10 @@ app.get('/api/search', (req, res) => {
 
 
 app.use('/api/users', usersRoutes)
+
+app.use('*', (req, res) => {
+	res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, (err) => {
     console.log(err || `Server running on port ${PORT}.`)
