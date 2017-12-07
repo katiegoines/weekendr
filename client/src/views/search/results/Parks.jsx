962a9da1203edbb08338e5085ae593b5cf6f690b
Katie Goines <katiegoines3@gmail.com>
2017-12-06 16:30:33 -0800

    Parks - replaced Google with Yelp

--- client/src/views/search/results/Parks.jsx
+++ client/src/views/search/results/Parks.jsx
@@ -24,21 +24,19 @@ class Parks extends React.Component {
     }
 
     request() {
-        axios({method: 'get', url: `/api/search/places?category=parks&lat=${this.props.lat}&lon=${this.props.lon}`})
+        axios({method: 'get', url: `/api/search/yelp?categories=parks&location=${this.props.search}`})
         .then((res) => { 
-            // console.log(res.data)
             if(res.data.fullType === "rest-call.response-filters.unhandled-status") {
                 throw new Error("error")
             } else {
-                // console.log(res.data.results)
-                this.key = res.data.apiKey
                 this.setState({results: {
-                        list: res.data.results.results, 
+                        list: res.data, 
                         head: "Parks"
                 }})
             }
         })
         .then(res => {
+            // console.log(this.state.results.list)
             this.randomizeColor()
         })
         .catch(e => {
@@ -62,21 +60,33 @@ class Parks extends React.Component {
                                 {this.state.results.head !== ''
                                     ? <div className="search-category">
                                         <h3>{this.state.results.head}</h3>
-                                        {/* <a href={``} target="_blank">See more results from Google</a> */}
+                                        <a href={`https://www.yelp.com/search?categories=parks&location=${this.props.search}&start=0&cflt=parks`} target="_blank">See more results from Yelp</a>
                                     </div>
                                     : null
                                 }
                                 {this.state.results.list.slice(0, this.props.quantity).map(el => {
                                     return (
                                         <div key={el.id} className="card-2">
-                                            <img className={`card-img-${this.randomizeColor()}`} src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference=${el.photos[0].photo_reference}&key=${this.key}`} alt="" />
+                                            <img className={`card-img-${this.randomizeColor()}`} src={el.image_url} alt="" />
                                             <div className={el.image !== null ? `card-overlay-${this.color}` : `card-overlay-${this.randomizeColor()}`}>
-                                                <div className="card-title">{el.name}</div>
-                                                <br />
+                                                <div className="card-title"><a href={el.url} target="_blank">{el.name}</a></div>
                                                 <div className="card-info">
+                                                    <div className="yelp-categories">{el.categories.map((cat, i)=> {
+                                                        return (
+                                                            <span key={i}>
+                                                                {i < el.categories.length - 1
+                                                                    ? cat.title + " - "
+                                                                    : cat.title
+                                                                }
+                                                            </span>
+                                                        )
+                                                    })}</div>
                                                     <div className="body-text">
-                                                        <div>{el.vicinity}</div>
-                                                        <div>{`Rating: ${el.rating}`}</div>
+                                                        <div>{el.location.address1}</div>
+                                                        <div>{el.location.city}</div>
+                                                        <div>{`${(el.distance * 0.000621371192).toFixed(2)}mi away`}</div>
+                                                        <div>{`Price: ${el.price}`}</div>
+                                                        <div>{`Rating: ${el.rating} (${el.review_count} reviews)`}</div>
                                                     </div>
                                                 </div>
                                             </div>
@@ -95,10 +105,21 @@ class Parks extends React.Component {
                         <h3>{this.state.results.head}</h3>
                         {this.state.results.list.slice(0, this.props.quantity).map(el => {
                             return (
-                                <div className="list-result" key={el.id}><span className="non-a">{el.name}</span>
+                                <div  className="list-result" key={el.id}><a href={el.url} target="_blank">{el.name}</a>
                                     <div>
-                                        <div>{el.vicinity}</div>
-                                        <div>{`Rating: ${el.rating}`}</div>
+                                        <div>
+                                            {el.categories.map((cat, i) => {
+                                                return (
+                                                    <span key={i}><small>
+                                                        {(i + 1) < el.categories.length ? <span>{`${cat.title}, `}</span> : <span>{cat.title}</span>}
+                                                    </small></span>
+                                                )
+                                            })}
+                                        </div>
+                                        <div>{`${el.location.address1}, ${el.location.city}`}</div>
+                                        <div>{`${(el.distance * 0.000621371192).toFixed(2)}mi away`}</div>
+                                        <div>{`Price: ${el.price}`}</div>
+                                        <div>{`Rating: ${el.rating} (${el.review_count} reviews)`}</div>
                                     </div>
                                 </div> 
                             )
